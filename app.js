308102a9c2f47043d6c20a1bd63286dfc880e483
melpiress <melissa.foneca@germinare.org.br>
2025-09-02 21:47:15 -0300

    configura o modo escuro e a nav bar responsiva

--- app.js
+++ app.js
@@ -6,7 +6,28 @@
 
 const navToggle = document.querySelector('.nav-toggle')
 const links = document.querySelector('.links')
+const moonBtn = document.getElementById('moon')
+const sunBtn = document.getElementById('sun')
+const body = document.querySelector('body')
 
 navToggle.addEventListener('click', function () {
   links.classList.toggle('show-links')
+})
+
+// ===== Dark Mode =====
+moonBtn.addEventListener('click', () => {
+    if (!body.classList.contains('dark_mode')){
+      body.classList.add('dark_mode')
+      moonBtn.src = 'img/dark_mode_moon.svg'
+      sunBtn.src = 'img/dark_mode_sun.svg'
+    }
+  }
+)
+
+sunBtn.addEventListener('click', () => {
+  if (body.classList.contains('dark_mode')){
+      body.classList.remove('dark_mode')
+      moonBtn.src = 'img/light_mode_moon.svg'
+      sunBtn.src = 'img/light_mode_sun.svg'
+    }
 })
